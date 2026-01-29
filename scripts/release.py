OpenHCSDev/ZMQRuntime

Add release scripts

scripts/release.py
@@ -1,0 +1,63 @@
+#!/usr/bin/env python3
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+import requests
+from packaging import version
+
+
+def get_current_version():
+    with open("src/zmqruntime/__init__.py", "r") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return line.split("=")[1].strip().strip('"\'')
+    return None
+
+def get_pypi_version():
+    try:
+        response = requests.get("https://pypi.org/pypi/zmqruntime/json")
+        if response.status_code == 200:
+            return response.json()["info"]["version"]
+    except:
+        pass
+    return None
+
+def main():
+    # Get current version
+    current_version = get_current_version()
+    if not current_version:
+        print("Error: Could not find version in __init__.py")
+        sys.exit(1)
+    
+    # Get PyPI version
+    pypi_version = get_pypi_version()
+    print(f"Current package version: {current_version}")
+    print(f"Current PyPI version: {pypi_version}")
+    
+    if pypi_version and version.parse(current_version) <= version.parse(pypi_version):
+        print(f"Error: Current version ({current_version}) must be greater than PyPI version ({pypi_version})")
+        sys.exit(1)
+    
+    # Confirm with user
+    response = input(f"Create release for v{current_version}? [y/N] ")
+    if response.lower() != 'y':
+        print("Aborted.")
+        return
+    
+    try:
+        # Create and push tag
+        subprocess.run(['git', 'tag', '-a', f'v{current_version}', '-m', f'Release version {current_version}'], check=True)
+        subprocess.run(['git', 'push', 'origin', f'v{current_version}'], check=True)
+        
+        print(f"\nSuccessfully created and pushed tag v{current_version}")
+        print("GitHub Actions workflow should start automatically.")
+        print("Monitor progress at: https://github.com/OpenHCSDev/zmqruntime/actions")
+    
+    except subprocess.CalledProcessError as e:
+        print(f"Error during release process: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()

scripts/update_version.py
@@ -1,0 +1,101 @@
+#!/usr/bin/env python3
+import argparse
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+from packaging import version
+
+
+def get_current_version():
+    """Get the current version from __init__.py"""
+    init_file = Path("src/zmqruntime/__init__.py")
+    content = init_file.read_text()
+    match = re.search(r'__version__\s*=\s*["\']([^"\']+)["\']', content)
+    return match.group(1) if match else None
+
+def validate_version(new_version):
+    """Validate the new version string"""
+    try:
+        v = version.parse(new_version)
+        if not isinstance(v, version.Version):
+            raise ValueError("Invalid version format")
+        return True
+    except version.InvalidVersion:
+        raise ValueError("Invalid version format. Use semantic versioning (e.g., 1.2.3)")
+
+def check_for_uncommitted_changes():
+    """Check if there are any uncommitted changes in tracked files"""
+    # Check for staged and unstaged changes in tracked files
+    staged = subprocess.run(['git', 'diff', '--staged', '--quiet'])
+    unstaged = subprocess.run(['git', 'diff', '--quiet'])
+    return staged.returncode != 0 or unstaged.returncode != 0
+
+def update_version(new_version=None):
+    """Update version in files and create git commit"""
+    try:
+        # Get current version first
+        current_version = get_current_version()
+        if not current_version:
+            print("Error: Could not find current version in __init__.py")
+            sys.exit(1)
+
+        if new_version is None:
+            # Auto-increment patch version if no version specified
+            v = version.parse(current_version)
+            new_version = f"{v.major}.{v.minor}.{v.micro + 1}"
+        
+        # Validate new version
+        validate_version(new_version)
+        if version.parse(new_version) <= version.parse(current_version):
+            print(f"Error: New version ({new_version}) must be greater than current version ({current_version})")
+            sys.exit(1)
+
+        # Check for uncommitted changes
+        if check_for_uncommitted_changes():
+            print("Error: You have uncommitted changes. Please commit or stash them first.")
+            sys.exit(1)
+
+        # Pull latest changes
+        try:
+            subprocess.run(['git', 'pull', 'origin', 'main'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error: Failed to pull latest changes: {e}")
+            sys.exit(1)
+        
+        # Update version in __init__.py
+        init_file = Path("src/zmqruntime/__init__.py")
+        content = init_file.read_text()
+        new_content = re.sub(
+            r'(__version__\s*=\s*[\'"])[^\'"]+([\'"])',
+            rf'\g<1>{new_version}\2',
+            content
+        )
+        init_file.write_text(new_content)
+
+        # Commit and push changes
+        subprocess.run(['git', 'add', 'src/zmqruntime/__init__.py'], check=True)
+        subprocess.run(['git', 'commit', '-m', f'bump version to {new_version}'], check=True)
+        subprocess.run(['git', 'push', 'origin', 'main'], check=True)
+        
+        print(f"Successfully updated version to {new_version}")
+        print("Now run: python scripts/release.py")
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Error during version update: {e}")
+        sys.exit(1)
+    except ValueError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+def main():
+    parser = argparse.ArgumentParser(description='Update zmqruntime version number')
+    parser.add_argument('--version', '-v',
+                      help='New version number (e.g., 1.2.3). If not provided, will increment patch version.')
+    args = parser.parse_args()
+
+    update_version(args.version)
+
+if __name__ == "__main__":
+    main()

scripts/verify_release_ready.py
@@ -1,0 +1,274 @@
+#!/usr/bin/env python3
+"""
+Verify that zmqruntime is ready for PyPI release.
+
+This script checks:
+- Version is valid
+- Package can be built
+- Metadata is correct
+- Dependencies are available
+"""
+
+import os
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+
+def check_version():
+    """Check that version is valid and follows semantic versioning."""
+    print("Checking version...")
+    init_file = Path("src/zmqruntime/__init__.py")
+    if not init_file.exists():
+        print("  ❌ src/zmqruntime/__init__.py not found")
+        return False
+    
+    content = init_file.read_text()
+    match = re.search(r'__version__\s*=\s*["\']([^"\']+)["\']', content)
+    if not match:
+        print("  ❌ __version__ not found in src/zmqruntime/__init__.py")
+        return False
+    
+    version = match.group(1)
+    # Basic semantic versioning check
+    if not re.match(r'^\d+\.\d+\.\d+', version):
+        print(f"  ❌ Version '{version}' doesn't follow semantic versioning (MAJOR.MINOR.PATCH)")
+        return False
+    
+    print(f"  ✅ Version: {version}")
+    return True
+
+
+def check_pyproject_toml():
+    """Check that pyproject.toml exists and has required fields."""
+    print("\nChecking pyproject.toml...")
+    pyproject_file = Path("pyproject.toml")
+    if not pyproject_file.exists():
+        print("  ❌ pyproject.toml not found")
+        return False
+
+    content = pyproject_file.read_text()
+    required_fields = {
+        'name': r'name\s*=\s*["\']openhcs["\']',
+        'version': r'version\s*=',
+        'description': r'description\s*=',
+        'authors': r'authors\s*=',
+        'build-backend': r'build-backend\s*=\s*["\']setuptools\.build_meta["\']',
+    }
+
+    all_found = True
+    for field, pattern in required_fields.items():
+        if not re.search(pattern, content):
+            print(f"  ❌ Missing or invalid field: {field}")
+            all_found = False
+
+    if all_found:
+        print("  ✅ All required fields present")
+    return all_found
+
+
+def check_readme():
+    """Check that README.md exists and is not empty."""
+    print("\nChecking README.md...")
+    readme_file = Path("README.md")
+    if not readme_file.exists():
+        print("  ❌ README.md not found")
+        return False
+    
+    content = readme_file.read_text()
+    if len(content.strip()) < 100:
+        print("  ⚠️  README.md seems very short")
+        return False
+    
+    print(f"  ✅ README.md exists ({len(content)} chars)")
+    return True
+
+
+def check_build_dependencies():
+    """Check that build dependencies are installed."""
+    print("\nChecking build dependencies...")
+    required = ['build', 'twine', 'packaging', 'requests']
+    missing = []
+    
+    for package in required:
+        try:
+            __import__(package)
+            print(f"  ✅ {package}")
+        except ImportError:
+            print(f"  ❌ {package} not installed")
+            missing.append(package)
+    
+    if missing:
+        print(f"\n  Install missing packages: pip install {' '.join(missing)}")
+        return False
+    return True
+
+
+def check_git_status():
+    """Check git status."""
+    print("\nChecking git status...")
+    try:
+        # Check if we're in a git repo
+        subprocess.run(['git', 'status'], capture_output=True, check=True)
+        
+        # Check for uncommitted changes
+        staged = subprocess.run(['git', 'diff', '--staged', '--quiet'], capture_output=True)
+        unstaged = subprocess.run(['git', 'diff', '--quiet'], capture_output=True)
+        
+        if staged.returncode != 0 or unstaged.returncode != 0:
+            print("  ⚠️  You have uncommitted changes")
+            print("     (This is OK if you plan to commit before release)")
+        else:
+            print("  ✅ Working directory clean")
+        
+        # Check current branch
+        result = subprocess.run(['git', 'branch', '--show-current'], 
+                              capture_output=True, text=True, check=True)
+        branch = result.stdout.strip()
+        if branch != 'main':
+            print(f"  ⚠️  Current branch is '{branch}', not 'main'")
+        else:
+            print(f"  ✅ On main branch")
+        
+        return True
+    except subprocess.CalledProcessError:
+        print("  ❌ Not a git repository or git not available")
+        return False
+
+
+def try_build():
+    """Try to build the package."""
+    print("\nTrying to build package...")
+    try:
+        # Clean old builds
+        import shutil
+        for dir_name in ['dist', 'build', 'zmqruntime.egg-info']:
+            if Path(dir_name).exists():
+                shutil.rmtree(dir_name)
+                print(f"  🧹 Cleaned {dir_name}/")
+        
+        # Build
+        result = subprocess.run(
+            ['python', '-m', 'build'],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        
+        # Check dist directory
+        dist_files = list(Path('dist').glob('*'))
+        if not dist_files:
+            print("  ❌ Build succeeded but no files in dist/")
+            return False
+        
+        print(f"  ✅ Build successful!")
+        print(f"     Created {len(dist_files)} files:")
+        for f in dist_files:
+            print(f"       - {f.name}")
+        
+        # Try to check with twine
+        try:
+            import glob
+            import shutil
+
+            # Check if twine is available
+            if not shutil.which('twine'):
+                print("  ⚠️  twine not found, skipping metadata check")
+                print("     Install with: pip install twine")
+                print("     (Build succeeded, but metadata not validated)")
+                return True  # Don't fail the build check just because twine is missing
+
+            dist_files = glob.glob('dist/*')
+            result = subprocess.run(
+                ['twine', 'check'] + dist_files,
+                capture_output=True,
+                text=True,
+                check=True
+            )
+            print("  ✅ Package metadata valid (twine check passed)")
+        except subprocess.CalledProcessError as e:
+            print("  ❌ Package metadata invalid:")
+            print(f"     {e.stderr}")
+            return False
+        
+        return True
+        
+    except subprocess.CalledProcessError as e:
+        print("  ❌ Build failed:")
+        print(f"     {e.stderr}")
+        return False
+    except Exception as e:
+        print(f"  ❌ Error during build: {e}")
+        return False
+
+
+def check_github_workflow():
+    """Check that GitHub Actions workflow exists."""
+    print("\nChecking GitHub Actions workflow...")
+    workflow_file = Path(".github/workflows/publish.yml")
+    if not workflow_file.exists():
+        print("  ❌ .github/workflows/publish.yml not found")
+        return False
+    
+    content = workflow_file.read_text()
+    if 'PYPI_API_TOKEN' not in content:
+        print("  ❌ PYPI_API_TOKEN not referenced in workflow")
+        return False
+    
+    print("  ✅ GitHub Actions workflow configured")
+    print("     Remember to set PYPI_API_TOKEN secret in GitHub!")
+    return True
+
+
+def main():
+    """Run all checks."""
+    print("=" * 60, flush=True)
+    print("zmqruntime PyPI Release Readiness Check", flush=True)
+    print("=" * 60, flush=True)
+    
+    checks = [
+        ("Version", check_version),
+        ("pyproject.toml", check_pyproject_toml),
+        ("README.md", check_readme),
+        ("Build dependencies", check_build_dependencies),
+        ("Git status", check_git_status),
+        ("GitHub workflow", check_github_workflow),
+        ("Package build", try_build),
+    ]
+    
+    results = {}
+    for name, check_func in checks:
+        try:
+            results[name] = check_func()
+        except Exception as e:
+            print(f"\n❌ Error checking {name}: {e}")
+            results[name] = False
+    
+    print("\n" + "=" * 60)
+    print("Summary")
+    print("=" * 60)
+    
+    passed = sum(results.values())
+    total = len(results)
+    
+    for name, result in results.items():
+        status = "✅" if result else "❌"
+        print(f"{status} {name}")
+    
+    print(f"\nPassed: {passed}/{total}")
+    
+    if passed == total:
+        print("\n🎉 All checks passed! Ready for release!")
+        print("\nNext steps:")
+        print("  1. Set PYPI_API_TOKEN in GitHub secrets")
+        print("  2. Run: python scripts/update_and_release.py")
+        return 0
+    else:
+        print("\n⚠️  Some checks failed. Please fix issues before releasing.")
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+

OpenHCSDev/python-introspect

scripts/release.py
@@ -1,0 +1,63 @@
+#!/usr/bin/env python3
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+import requests
+from packaging import version
+
+
+def get_current_version():
+    with open("src/python_introspect/__init__.py", "r") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return line.split("=")[1].strip().strip('"\'')
+    return None
+
+def get_pypi_version():
+    try:
+        response = requests.get("https://pypi.org/pypi/python-introspect/json")
+        if response.status_code == 200:
+            return response.json()["info"]["version"]
+    except:
+        pass
+    return None
+
+def main():
+    # Get current version
+    current_version = get_current_version()
+    if not current_version:
+        print("Error: Could not find version in __init__.py")
+        sys.exit(1)
+    
+    # Get PyPI version
+    pypi_version = get_pypi_version()
+    print(f"Current package version: {current_version}")
+    print(f"Current PyPI version: {pypi_version}")
+    
+    if pypi_version and version.parse(current_version) <= version.parse(pypi_version):
+        print(f"Error: Current version ({current_version}) must be greater than PyPI version ({pypi_version})")
+        sys.exit(1)
+    
+    # Confirm with user
+    response = input(f"Create release for v{current_version}? [y/N] ")
+    if response.lower() != 'y':
+        print("Aborted.")
+        return
+    
+    try:
+        # Create and push tag
+        subprocess.run(['git', 'tag', '-a', f'v{current_version}', '-m', f'Release version {current_version}'], check=True)
+        subprocess.run(['git', 'push', 'origin', f'v{current_version}'], check=True)
+        
+        print(f"\nSuccessfully created and pushed tag v{current_version}")
+        print("GitHub Actions workflow should start automatically.")
+        print("Monitor progress at: https://github.com/OpenHCSDev/python-introspect/actions")
+    
+    except subprocess.CalledProcessError as e:
+        print(f"Error during release process: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()

scripts/update_version.py
@@ -1,0 +1,101 @@
+#!/usr/bin/env python3
+import argparse
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+from packaging import version
+
+
+def get_current_version():
+    """Get the current version from __init__.py"""
+    init_file = Path("src/python_introspect/__init__.py")
+    content = init_file.read_text()
+    match = re.search(r'__version__\s*=\s*["\']([^"\']+)["\']', content)
+    return match.group(1) if match else None
+
+def validate_version(new_version):
+    """Validate the new version string"""
+    try:
+        v = version.parse(new_version)
+        if not isinstance(v, version.Version):
+            raise ValueError("Invalid version format")
+        return True
+    except version.InvalidVersion:
+        raise ValueError("Invalid version format. Use semantic versioning (e.g., 1.2.3)")
+
+def check_for_uncommitted_changes():
+    """Check if there are any uncommitted changes in tracked files"""
+    # Check for staged and unstaged changes in tracked files
+    staged = subprocess.run(['git', 'diff', '--staged', '--quiet'])
+    unstaged = subprocess.run(['git', 'diff', '--quiet'])
+    return staged.returncode != 0 or unstaged.returncode != 0
+
+def update_version(new_version=None):
+    """Update version in files and create git commit"""
+    try:
+        # Get current version first
+        current_version = get_current_version()
+        if not current_version:
+            print("Error: Could not find current version in __init__.py")
+            sys.exit(1)
+
+        if new_version is None:
+            # Auto-increment patch version if no version specified
+            v = version.parse(current_version)
+            new_version = f"{v.major}.{v.minor}.{v.micro + 1}"
+        
+        # Validate new version
+        validate_version(new_version)
+        if version.parse(new_version) <= version.parse(current_version):
+            print(f"Error: New version ({new_version}) must be greater than current version ({current_version})")
+            sys.exit(1)
+
+        # Check for uncommitted changes
+        if check_for_uncommitted_changes():
+            print("Error: You have uncommitted changes. Please commit or stash them first.")
+            sys.exit(1)
+
+        # Pull latest changes
+        try:
+            subprocess.run(['git', 'pull', 'origin', 'main'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error: Failed to pull latest changes: {e}")
+            sys.exit(1)
+        
+        # Update version in __init__.py
+        init_file = Path("src/python_introspect/__init__.py")
+        content = init_file.read_text()
+        new_content = re.sub(
+            r'(__version__\s*=\s*[\'"])[^\'"]+([\'"])',
+            rf'\g<1>{new_version}\2',
+            content
+        )
+        init_file.write_text(new_content)
+
+        # Commit and push changes
+        subprocess.run(['git', 'add', 'src/python_introspect/__init__.py'], check=True)
+        subprocess.run(['git', 'commit', '-m', f'bump version to {new_version}'], check=True)
+        subprocess.run(['git', 'push', 'origin', 'main'], check=True)
+        
+        print(f"Successfully updated version to {new_version}")
+        print("Now run: python scripts/release.py")
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Error during version update: {e}")
+        sys.exit(1)
+    except ValueError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+def main():
+    parser = argparse.ArgumentParser(description='Update python-introspect version number')
+    parser.add_argument('--version', '-v',
+                      help='New version number (e.g., 1.2.3). If not provided, will increment patch version.')
+    args = parser.parse_args()
+
+    update_version(args.version)
+
+if __name__ == "__main__":
+    main()

scripts/verify_release_ready.py
@@ -1,0 +1,274 @@
+#!/usr/bin/env python3
+"""
+Verify that python-introspect is ready for PyPI release.
+
+This script checks:
+- Version is valid
+- Package can be built
+- Metadata is correct
+- Dependencies are available
+"""
+
+import os
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+
+def check_version():
+    """Check that version is valid and follows semantic versioning."""
+    print("Checking version...")
+    init_file = Path("src/python_introspect/__init__.py")
+    if not init_file.exists():
+        print("  ❌ src/python_introspect/__init__.py not found")
+        return False
+    
+    content = init_file.read_text()
+    match = re.search(r'__version__\s*=\s*["\']([^"\']+)["\']', content)
+    if not match:
+        print("  ❌ __version__ not found in src/python_introspect/__init__.py")
+        return False
+    
+    version = match.group(1)
+    # Basic semantic versioning check
+    if not re.match(r'^\d+\.\d+\.\d+', version):
+        print(f"  ❌ Version '{version}' doesn't follow semantic versioning (MAJOR.MINOR.PATCH)")
+        return False
+    
+    print(f"  ✅ Version: {version}")
+    return True
+
+
+def check_pyproject_toml():
+    """Check that pyproject.toml exists and has required fields."""
+    print("\nChecking pyproject.toml...")
+    pyproject_file = Path("pyproject.toml")
+    if not pyproject_file.exists():
+        print("  ❌ pyproject.toml not found")
+        return False
+
+    content = pyproject_file.read_text()
+    required_fields = {
+        'name': r'name\s*=\s*["\']openhcs["\']',
+        'version': r'version\s*=',
+        'description': r'description\s*=',
+        'authors': r'authors\s*=',
+        'build-backend': r'build-backend\s*=\s*["\']setuptools\.build_meta["\']',
+    }
+
+    all_found = True
+    for field, pattern in required_fields.items():
+        if not re.search(pattern, content):
+            print(f"  ❌ Missing or invalid field: {field}")
+            all_found = False
+
+    if all_found:
+        print("  ✅ All required fields present")
+    return all_found
+
+
+def check_readme():
+    """Check that README.md exists and is not empty."""
+    print("\nChecking README.md...")
+    readme_file = Path("README.md")
+    if not readme_file.exists():
+        print("  ❌ README.md not found")
+        return False
+    
+    content = readme_file.read_text()
+    if len(content.strip()) < 100:
+        print("  ⚠️  README.md seems very short")
+        return False
+    
+    print(f"  ✅ README.md exists ({len(content)} chars)")
+    return True
+
+
+def check_build_dependencies():
+    """Check that build dependencies are installed."""
+    print("\nChecking build dependencies...")
+    required = ['build', 'twine', 'packaging', 'requests']
+    missing = []
+    
+    for package in required:
+        try:
+            __import__(package)
+            print(f"  ✅ {package}")
+        except ImportError:
+            print(f"  ❌ {package} not installed")
+            missing.append(package)
+    
+    if missing:
+        print(f"\n  Install missing packages: pip install {' '.join(missing)}")
+        return False
+    return True
+
+
+def check_git_status():
+    """Check git status."""
+    print("\nChecking git status...")
+    try:
+        # Check if we're in a git repo
+        subprocess.run(['git', 'status'], capture_output=True, check=True)
+        
+        # Check for uncommitted changes
+        staged = subprocess.run(['git', 'diff', '--staged', '--quiet'], capture_output=True)
+        unstaged = subprocess.run(['git', 'diff', '--quiet'], capture_output=True)
+        
+        if staged.returncode != 0 or unstaged.returncode != 0:
+            print("  ⚠️  You have uncommitted changes")
+            print("     (This is OK if you plan to commit before release)")
+        else:
+            print("  ✅ Working directory clean")
+        
+        # Check current branch
+        result = subprocess.run(['git', 'branch', '--show-current'], 
+                              capture_output=True, text=True, check=True)
+        branch = result.stdout.strip()
+        if branch != 'main':
+            print(f"  ⚠️  Current branch is '{branch}', not 'main'")
+        else:
+            print(f"  ✅ On main branch")
+        
+        return True
+    except subprocess.CalledProcessError:
+        print("  ❌ Not a git repository or git not available")
+        return False
+
+
+def try_build():
+    """Try to build the package."""
+    print("\nTrying to build package...")
+    try:
+        # Clean old builds
+        import shutil
+        for dir_name in ['dist', 'build', 'python-introspect.egg-info']:
+            if Path(dir_name).exists():
+                shutil.rmtree(dir_name)
+                print(f"  🧹 Cleaned {dir_name}/")
+        
+        # Build
+        result = subprocess.run(
+            ['python', '-m', 'build'],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        
+        # Check dist directory
+        dist_files = list(Path('dist').glob('*'))
+        if not dist_files:
+            print("  ❌ Build succeeded but no files in dist/")
+            return False
+        
+        print(f"  ✅ Build successful!")
+        print(f"     Created {len(dist_files)} files:")
+        for f in dist_files:
+            print(f"       - {f.name}")
+        
+        # Try to check with twine
+        try:
+            import glob
+            import shutil
+
+            # Check if twine is available
+            if not shutil.which('twine'):
+                print("  ⚠️  twine not found, skipping metadata check")
+                print("     Install with: pip install twine")
+                print("     (Build succeeded, but metadata not validated)")
+                return True  # Don't fail the build check just because twine is missing
+
+            dist_files = glob.glob('dist/*')
+            result = subprocess.run(
+                ['twine', 'check'] + dist_files,
+                capture_output=True,
+                text=True,
+                check=True
+            )
+            print("  ✅ Package metadata valid (twine check passed)")
+        except subprocess.CalledProcessError as e:
+            print("  ❌ Package metadata invalid:")
+            print(f"     {e.stderr}")
+            return False
+        
+        return True
+        
+    except subprocess.CalledProcessError as e:
+        print("  ❌ Build failed:")
+        print(f"     {e.stderr}")
+        return False
+    except Exception as e:
+        print(f"  ❌ Error during build: {e}")
+        return False
+
+
+def check_github_workflow():
+    """Check that GitHub Actions workflow exists."""
+    print("\nChecking GitHub Actions workflow...")
+    workflow_file = Path(".github/workflows/publish.yml")
+    if not workflow_file.exists():
+        print("  ❌ .github/workflows/publish.yml not found")
+        return False
+    
+    content = workflow_file.read_text()
+    if 'PYPI_API_TOKEN' not in content:
+        print("  ❌ PYPI_API_TOKEN not referenced in workflow")
+        return False
+    
+    print("  ✅ GitHub Actions workflow configured")
+    print("     Remember to set PYPI_API_TOKEN secret in GitHub!")
+    return True
+
+
+def main():
+    """Run all checks."""
+    print("=" * 60, flush=True)
+    print("python-introspect PyPI Release Readiness Check", flush=True)
+    print("=" * 60, flush=True)
+    
+    checks = [
+        ("Version", check_version),
+        ("pyproject.toml", check_pyproject_toml),
+        ("README.md", check_readme),
+        ("Build dependencies", check_build_dependencies),
+        ("Git status", check_git_status),
+        ("GitHub workflow", check_github_workflow),
+        ("Package build", try_build),
+    ]
+    
+    results = {}
+    for name, check_func in checks:
+        try:
+            results[name] = check_func()
+        except Exception as e:
+            print(f"\n❌ Error checking {name}: {e}")
+            results[name] = False
+    
+    print("\n" + "=" * 60)
+    print("Summary")
+    print("=" * 60)
+    
+    passed = sum(results.values())
+    total = len(results)
+    
+    for name, result in results.items():
+        status = "✅" if result else "❌"
+        print(f"{status} {name}")
+    
+    print(f"\nPassed: {passed}/{total}")
+    
+    if passed == total:
+        print("\n🎉 All checks passed! Ready for release!")
+        print("\nNext steps:")
+        print("  1. Set PYPI_API_TOKEN in GitHub secrets")
+        print("  2. Run: python scripts/update_and_release.py")
+        return 0
+    else:
+        print("\n⚠️  Some checks failed. Please fix issues before releasing.")
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+